a5216740b829328cefb51c435867ca0a7f1ccaee
gautamamber <ambergautam1@gmail.com>
2020-02-22 00:27:35 +0530

    feat():add recipe detail serializer

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -40,3 +40,10 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
                   'price', 'link')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredeientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+
