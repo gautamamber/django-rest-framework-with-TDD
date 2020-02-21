7268efee052cd8ad7eff91462a0fa951e605d015
gautamamber <ambergautam1@gmail.com>
2020-02-21 21:08:43 +0530

    feat():add tag module for recipe

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,5 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from .. import models
+
+
+def sample_user(email="amber@test.com", password="test123"):
+    """
+    Create a sample user
+    :param email:
+    :param password:
+    :return:
+    """
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -48,3 +59,11 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """
+        Test the tag string representation
+        :return:
+        """
+        tag = models.Tag.objects.create(user=sample_user(), name="Vegan")
+        self.assertEqual(tag.name, "Vegan")
